0b84e2a3bb423cf04186ebaaf2c84bfcd6ec9629
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-04-21 18:18:19 +0300

    Mutable data in record in CollectedMetrics.ml

--- src/CollectedMetrics.ml
+++ src/CollectedMetrics.ml
@@ -2,6 +2,20 @@ open Base
 open Caml.Format
 open Utils
 
+type context = {
+    file_list : string Queue.t;
+    functions_in_file : (string, string list) Hashtbl.t;
+    metric_results : (string, (string * float) list) Hashtbl.t;
+    metric_extra_info : (string, string list) Hashtbl.t
+}
+
+let ctx : context = {
+    file_list = Queue.create ();
+    functions_in_file = Hashtbl.create (module String);
+    metric_results = Hashtbl.create (module String);
+    metric_extra_info = Hashtbl.create (module String)
+}
+
 let add_value table key value =
   Hashtbl.update table key ~f:(fun v ->
       match v with
@@ -9,20 +23,12 @@ let add_value table key value =
       | Some list -> value :: list)
 ;;
 
-let file_list : string Queue.t = Queue.create ()
-let add_file = Queue.enqueue file_list
-let functions_in_file : (string, string list) Hashtbl.t = Hashtbl.create (module String)
-let add_function filename func = add_value functions_in_file filename func
-
-let metric_results : (string, (string * float) list) Hashtbl.t =
-  Hashtbl.create (module String)
-;;
-
-let add_result metric_id where res = add_value metric_results where (metric_id, res)
-let metric_extra_info : (string, string list) Hashtbl.t = Hashtbl.create (module String)
+let add_file = Queue.enqueue ctx.file_list
+let add_function filename func = add_value ctx.functions_in_file filename func
+let add_result metric_id where res = add_value ctx.metric_results where (metric_id, res)
 
 let add_extra_info where extra_info =
-  Hashtbl.update metric_extra_info where ~f:(fun v ->
+  Hashtbl.update ctx.metric_extra_info where ~f:(fun v ->
       match v with
       | None -> extra_info
       | Some list -> list @ ("" :: extra_info))
@@ -31,7 +37,7 @@ let add_extra_info where extra_info =
 let print_extra_info verbose where =
   if verbose
   then (
-    match Hashtbl.find metric_extra_info where with
+    match Hashtbl.find ctx.metric_extra_info where with
     | None | Some [] -> ()
     | Some x ->
       Format.printf "\n_______extra_info_______\n\n";
@@ -43,7 +49,7 @@ let print_metric metric_id value = Format.printf "%s: %.2f\n" metric_id value
 
 let print_func_metrics verbose filename func =
   let key = filename ^ ":" ^ func in
-  let metrics = Hashtbl.find_exn metric_results key in
+  let metrics = Hashtbl.find_exn ctx.metric_results key in
   Format.printf "FUNCTION %s in %s\n" func filename;
   List.iter metrics ~f:(fun (x, y) -> print_metric x y);
   print_extra_info verbose key;
@@ -57,8 +63,8 @@ let default_find dict key =
 ;;
 
 let print_file_metrics verbose filename =
-  let metrics = default_find metric_results filename in
-  let functions = default_find functions_in_file filename in
+  let metrics = default_find ctx.metric_results filename in
+  let functions = default_find ctx.functions_in_file filename in
   Format.printf "FILE %s\n" filename;
   Format.printf "\n______File_metrics______\n\n";
   List.iter metrics ~f:(fun (x, y) -> print_metric x y);
@@ -70,4 +76,4 @@ let print_file_metrics verbose filename =
   Format.printf "\n"
 ;;
 
-let report verbose () = Queue.iter file_list ~f:(print_file_metrics verbose)
+let report verbose () = Queue.iter ctx.file_list ~f:(print_file_metrics verbose)
