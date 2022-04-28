5a602bf65d72d52c4de551abc40223360d1dc244
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-04-28 18:31:06 +0300

    My_Tast_iterator module

---
+++ src/My_Tast_iterator.ml
@@ -0,0 +1,91 @@
+open Caml
+open Base
+open Zanuda_core
+open Utils
+open Tast_iterator
+open Typedtree
+
+type iterator_params =
+  { filename : string
+  ; groups_of_metrics : (module METRIC.GROUP) list
+  ; metrics_to_show : string list
+  ; verbose_metrics : string list
+  }
+
+let before_function info func_info =
+  List.iter info.groups_of_metrics ~f:(fun (module L : METRIC.GROUP) ->
+      L.before_function func_info)
+;;
+
+let collect_results
+    info
+    ~metrics_group_id
+    ~get_result
+    ~get_extra_info
+    ~add_result
+    ~add_extra_info
+  =
+  List.iter (get_result ()) ~f:(fun (str, value) ->
+      let cur_metrics = metrics_group_id ^ "_" ^ str in
+      if List.exists info.metrics_to_show ~f:(fun x ->
+             String.is_substring cur_metrics ~substring:x)
+      then add_result cur_metrics value);
+  if List.mem info.verbose_metrics metrics_group_id ~equal:String.equal
+  then add_extra_info (get_extra_info ())
+;;
+
+let collect_function_results info func_name (module L : METRIC.GROUP) =
+  collect_results
+    info
+    ~metrics_group_id:L.metrics_group_id
+    ~get_result:L.get_function_metrics_result
+    ~get_extra_info:L.get_function_extra_info
+    ~add_result:(CollectedMetrics.add_func_result info.filename func_name)
+    ~add_extra_info:(CollectedMetrics.add_extra_info_func info.filename func_name)
+;;
+
+let collect_module_results info (module L : METRIC.GROUP) =
+  collect_results
+    info
+    ~metrics_group_id:L.metrics_group_id
+    ~get_result:L.get_module_metrics_result
+    ~get_extra_info:L.get_module_extra_info
+    ~add_result:(CollectedMetrics.add_file_result info.filename)
+    ~add_extra_info:(CollectedMetrics.add_extra_info_file info.filename)
+;;
+
+let collect_function_metrics info func_name =
+  List.iter info.groups_of_metrics ~f:(collect_function_results info func_name)
+;;
+
+let get_value_name vb =
+  let loc = short_location_str vb.vb_loc in
+  match get_vb_name vb with
+  | Some x -> Format.sprintf "%s <%s>" (Ident.name x) loc
+  | None -> Format.sprintf "<Value on %s>" loc
+;;
+
+let function_value_binding info func_info self x =
+  let value_name = get_value_name x in
+  CollectedMetrics.add_function info.filename value_name;
+  before_function info func_info;
+  self.value_binding self x;
+  collect_function_metrics info value_name
+;;
+
+let my_structure_item info self str_item =
+  match str_item.str_desc with
+  | Tstr_value (rec_flag, list) ->
+    List.iter list ~f:(fun x ->
+        if x.vb_loc.loc_ghost
+        then self.value_binding self x
+        else (
+          let func_info = { is_rec = rec_flag_to_bool rec_flag; name = get_vb_name x } in
+          function_value_binding info func_info self x))
+  | _ -> default_iterator.structure_item self str_item
+;;
+
+let my_iterator info =
+  let open Typedtree in
+  { default_iterator with structure_item = my_structure_item info }
+;;
--- src/main.ml
+++ src/main.ml
@@ -15,11 +15,14 @@ let groups_of_metrics =
 
 let metrics_group_id_list =
   List.map groups_of_metrics ~f:(fun (module L : METRIC.GROUP) -> L.metrics_group_id)
-  |> List.filter ~f:(fun x -> not @@ String.equal x "(test)")
 ;;
 
-let verbose_metrics = ref metrics_group_id_list
-let metrics_to_show = ref metrics_group_id_list
+let init_show_list =
+  metrics_group_id_list |> List.filter ~f:(fun x -> not @@ String.equal x "(test)")
+;;
+
+let verbose_metrics = ref init_show_list
+let metrics_to_show = ref init_show_list
 
 let change_metrics_lists new_verb_metrics new_metrics_to_show =
   let change list new_list =
@@ -31,79 +34,6 @@ let change_metrics_lists new_verb_metrics new_metrics_to_show =
   change metrics_to_show new_metrics_to_show
 ;;
 
-let before_function func_info =
-  List.iter groups_of_metrics ~f:(fun (module L : METRIC.GROUP) ->
-      L.before_function func_info)
-;;
-
-let collect_results
-    ~metrics_group_id
-    ~get_result
-    ~get_extra_info
-    ~add_result
-    ~add_extra_info
-  =
-  List.iter (get_result ()) ~f:(fun (str, value) ->
-      let cur_metrics = metrics_group_id ^ "_" ^ str in
-      if List.exists !metrics_to_show ~f:(fun x ->
-             String.is_substring cur_metrics ~substring:x)
-      then add_result cur_metrics value);
-  if List.mem !verbose_metrics metrics_group_id ~equal:String.equal
-  then add_extra_info (get_extra_info ())
-;;
-
-let collect_function_results filename func_name (module L : METRIC.GROUP) =
-  collect_results
-    ~metrics_group_id:L.metrics_group_id
-    ~get_result:L.get_function_metrics_result
-    ~get_extra_info:L.get_function_extra_info
-    ~add_result:(CollectedMetrics.add_func_result filename func_name)
-    ~add_extra_info:(CollectedMetrics.add_extra_info_func filename func_name)
-;;
-
-let collect_module_results filename (module L : METRIC.GROUP) =
-  collect_results
-    ~metrics_group_id:L.metrics_group_id
-    ~get_result:L.get_module_metrics_result
-    ~get_extra_info:L.get_module_extra_info
-    ~add_result:(CollectedMetrics.add_file_result filename)
-    ~add_extra_info:(CollectedMetrics.add_extra_info_file filename)
-;;
-
-let collect_function_metrics filename func_name =
-  List.iter groups_of_metrics ~f:(collect_function_results filename func_name)
-;;
-
-let init_iterator filename =
-  let open Typedtree in
-  let get_value_name vb =
-    let loc = short_location_str vb.vb_loc in
-    match get_vb_name vb with
-    | Some x -> Format.sprintf "%s <%s>" (Ident.name x) loc
-    | None -> Format.sprintf "<Value on %s>" loc
-  in
-  let open Tast_iterator in
-  let function_value_binding rec_flag self x =
-    let value_name = get_value_name x in
-    let func_info = { is_rec = rec_flag_to_bool rec_flag; name = get_vb_name x } in
-    CollectedMetrics.add_function filename value_name;
-    before_function func_info;
-    self.value_binding self x;
-    collect_function_metrics filename value_name
-  in
-  { default_iterator with
-    structure_item =
-      (fun self str_item ->
-        match str_item.str_desc with
-        | Tstr_value (rec_flag, list) ->
-          List.iter list ~f:(fun x ->
-              if empty_loc x.vb_loc
-              then self.value_binding self x
-              else function_value_binding rec_flag self x)
-        | _ -> default_iterator.structure_item self str_item)
-  }
-;;
-
 let build_iterator ~init ~compose ~f xs =
   let o = List.fold_left ~f:(fun acc lint -> compose lint acc) ~init xs in
   f o
@@ -111,19 +41,27 @@ let build_iterator ~init ~compose ~f xs =
 
 let typed_on_structure info file_content typedtree =
   let open Compile_common in
+  let open My_Tast_iterator in
   let filename = cut_build_dir info.source_file in
+  let iter_info =
+    { filename
+    ; groups_of_metrics
+    ; metrics_to_show = !metrics_to_show
+    ; verbose_metrics = !verbose_metrics
+    }
+  in
   build_iterator
     ~f:(fun o -> o.Tast_iterator.structure o)
     ~compose:(fun (module L : METRIC.GROUP) ->
       L.reset ();
       L.run info file_content)
-    ~init:(init_iterator filename)
+    ~init:(my_iterator iter_info)
     groups_of_metrics
     typedtree;
   build_iterator
     ~f:(fun () -> CollectedMetrics.add_file filename)
     ~compose:(fun (module L : METRIC.GROUP) () ->
-      collect_module_results filename (module L))
+      My_Tast_iterator.collect_module_results iter_info (module L))
     ~init:()
     groups_of_metrics
 ;;
